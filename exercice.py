a287c87ec295da04c8caf9b7900b41d5d33de9d6
kegaub <kegaub@users.noreply.github.com>
2021-01-14 08:55:28 -0500

    Initial commit

---
+++ exercice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# TODO: Importez vos modules ici
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# TODO: Définissez vos fonctions ici
+
+
+def preprocess_data(data_path: str="./data/winequality-white.csv") -> tuple:
+    df = pd.read_csv(data_path, sep=";", header=0)
+    y = df["quality"]
+    X = df.drop(columns=["quality"])
+
+    return train_test_split(X, y, test_size=0.1)
+
+
+if __name__ == '__main__':
+    # TODO: Appelez vos fonctions ici
+    X_train, X_test, y_train, y_test = preprocess_data()
